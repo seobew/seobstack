b6e983b8607268919380ba8124044fdd96960f81
seob <seobew@gmail.com>
2021-07-12 16:52:17 +0900

    Add card comp

--- libs/design-system/src/index.tsx
+++ libs/design-system/src/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from './Header';
 import { Thing } from './Thing';
 import { Sidebar } from './Siderbar';
+import { Card } from './Card';
 // export * from '.';
 // import { Sidebar } from './Siderbar';
 
-export { Header, Thing, Sidebar };
+export { Header, Thing, Sidebar, Card };
