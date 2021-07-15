6380260ea4cc02b2b4f80c73730102bc9e7aee26
seob <seobew@gmail.com>
2021-07-15 10:07:26 +0900

    feat(landing): update header to 20

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -5,7 +5,7 @@ import { Card, Layout } from "@seob/ds";
 function App() {
   return (
     <div className="App">
-      <div>this is test - 19</div>
+      <div>this is test - 20</div>
       <Layout>
         <Card />
       </Layout>
